602bcb494908a2a2cdb8e5bace2b33a78b598f64
potemkinViktor <viktorapotemkin@gmail.com>
2022-05-22 23:53:43 +0300

    .

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-["truffle-contract-size"]
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
